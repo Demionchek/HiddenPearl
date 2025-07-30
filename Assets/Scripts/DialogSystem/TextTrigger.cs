a324d4331e3367b263ca0dccae31fa39f08dafd3
Demionchek <Demionchek@gmail.com>
2025-07-30 11:29:01 +0300

    added text trigger

---
+++ Assets/Scripts/DialogSystem/TextTrigger.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using System.Collections;
+using TMPro;
+
+namespace DialogSystem
+{
+    public class TextTrigger : MonoBehaviour
+    {
+        [SerializeField] private GameObject textPanel;
+        [SerializeField] private TMP_Text textDisplay;
+        [SerializeField] private string message = "Текст сообщения";
+        [SerializeField] private float displaySpeed = 0.05f;
+
+        private Coroutine displayCoroutine;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.layer == LayerMask.NameToLayer("Player")) // Проверяем, что вошёл игрок
+            {
+                textPanel.SetActive(true); // Включаем панель
+                if (displayCoroutine != null)
+                {
+                    StopCoroutine(displayCoroutine);
+                }
+                displayCoroutine = StartCoroutine(DisplayText());
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.gameObject.layer == LayerMask.NameToLayer("Player")) // Проверяем, что вышел игрок
+            {
+                if (displayCoroutine != null)
+                {
+                    StopCoroutine(displayCoroutine);
+                }
+                textDisplay.text = ""; // Очищаем текст
+                textPanel.SetActive(false); // Выключаем панель
+            }
+        }
+
+        private IEnumerator DisplayText()
+        {
+            textDisplay.text = ""; // Очищаем текст перед началом
+
+            // Постепенно выводим текст посимвольно
+            foreach (char letter in message.ToCharArray())
+            {
+                textDisplay.text += letter;
+                yield return new WaitForSeconds(displaySpeed);
+            }
+        }
+    }
+}
